b7901eb159d9670af450cddff1311079a608dc11
MaxDesarrollo <maxdesarrollo@outlook.com>
2017-04-12 19:02:37 -0400

    videoActivity Consumiendo Api Youtube

--- app/src/main/java/com/breakstudio/casadeoracion/VideosActivity.java
+++ app/src/main/java/com/breakstudio/casadeoracion/VideosActivity.java
@@ -1,13 +1,80 @@
 package com.breakstudio.casadeoracion;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class VideosActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
+public class VideosActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener,YouTubePlayer.PlaybackEventListener {
+
+    String claveYoutube = "AIzaSyBSrqZ4W4pycsZ9IyUCz_txQ2Feu6sFaoI";
+    YouTubePlayerView youTubePlayerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_videos);
+
+        youTubePlayerView = (YouTubePlayerView)findViewById(R.id.youtube_view);
+        youTubePlayerView.initialize(claveYoutube,this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean fueRestaurado) {
+        if(!fueRestaurado){
+            youTubePlayer.cuePlaylist("PLbGtiIo49e8iBx_uoSt3-mxdsWSc_AVOG"); //apostolMoises
+        }
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+        if(youTubeInitializationResult.isUserRecoverableError()){
+            youTubeInitializationResult.getErrorDialog(this,1).show();
+        }else{
+            String error = "Error al iniciaizar youtube" + youTubeInitializationResult.toString();
+            Toast.makeText(getApplication(),error,Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected  void onActivityResult(int requestCode, int resultcode, Intent data){
+        if (requestCode ==1){
+            getYoutubePlayerProvider().initialize(claveYoutube,this);
+
+        }
+    }
+
+    protected  YouTubePlayer.Provider getYoutubePlayerProvider(){
+        return  youTubePlayerView;
+    }
+
+    @Override
+    public void onPlaying() {
+
+    }
+
+    @Override
+    public void onPaused() {
+
+    }
+
+    @Override
+    public void onStopped() {
+
+    }
+
+    @Override
+    public void onBuffering(boolean b) {
+
+    }
+
+    @Override
+    public void onSeekTo(int i) {
+
     }
 }
