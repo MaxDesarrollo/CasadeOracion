b62d0bd86d9f1a5e74b9598901d7e621e82bdf10
Jaime <zagaljj@gmail.com>
2017-05-16 16:50:09 -0400

    Agregada Base de datos de la biblia y clase de conexion

---
+++ app/src/main/java/com/breakstudio/casadeoracion/DatabaseAdapter.java
@@ -0,0 +1,44 @@
+package com.breakstudio.casadeoracion;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Jaime on 16/5/2017.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+
+    public static final String DBNAME = "Moderna.sqlite";
+    public static final String DBLOCATION ="/data/data/com.breakstudio.casadeoracion/databases";
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
+
+    public DatabaseHelper(Context context){
+        super(context,DBNAME,null,1);
+        this.mContext = context;
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+    public void openDatabase(){
+        String dbPath = mContext.getDatabasePath(DBNAME).getPath();
+        if(mDatabase!=null && mDatabase.isOpen()){
+            return;
+        }
+        mDatabase= SQLiteDatabase.openDatabase(dbPath,null,SQLiteDatabase.OPEN_READWRITE);
+    }
+    public void closeDatabase(){
+        if (mDatabase!=null){
+            mDatabase.close();
+        }
+    }
+    
+}
